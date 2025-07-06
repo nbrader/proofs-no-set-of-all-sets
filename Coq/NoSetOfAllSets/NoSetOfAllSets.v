0b019a3327dbe63ec4d3ecdfcf94ab2aaf6922eb
nbrader <nbrader+github@gmail.com>
2025-07-06 01:30:32 +0100

    Made some progress toward proving no universal set exists.

--- Coq/NoSetOfAllSets/NoSetOfAllSets.v
+++ Coq/NoSetOfAllSets/NoSetOfAllSets.v
@@ -2,23 +2,27 @@ Axiom SetT : Type.
 Axiom elementOf : SetT -> SetT -> Prop.
 Infix "∈" := elementOf (at level 70).
 
-Axiom extensionality : forall A B, (A = B <-> (forall x, x ∈ A <-> x ∈ B)).
+Axiom extensionality : forall a b, (a = b -> (forall x, x ∈ a <-> x ∈ b)).
 
 Axiom empty_set : SetT.
-Axiom empty_spec : forall x, ~ x ∈ empty_set.
+Notation "∅" := empty_set.
+Axiom empty_spec : forall x, ~ x ∈ ∅.
 
-Axiom union : SetT -> SetT -> SetT.
-Axiom union_spec : forall x a b, elementOf x (union a b) <-> x = a \/ x = b.
+Axiom pairing : forall x y, exists z, x ∈ z /\ y ∈ z.
 
 Definition subset : SetT -> SetT -> Prop := fun A B => forall x, x ∈ A -> x ∈ B.
 Infix "⊆" := subset (at level 70).
 
-Axiom powerset : SetT -> SetT.
-Axiom powerset_spec : forall x a, x ⊆ a <-> x ∈ powerset a.
+Axiom intersection : SetT -> SetT -> SetT.
+Infix "∩" := intersection (at level 70).
+Axiom intersection_spec : forall a b x, x ∈ (a ∩ b) <-> x = a /\ x = b.
 
-Theorem theorem1 : exists x : SetT, elementOf empty_set x /\ ~(empty_set = x).
+Axiom powerset : SetT -> SetT.
+Axiom powerset_spec : forall a x, x ⊆ a <-> x ∈ powerset a.
+
+Theorem theorem1 : exists x : SetT, ∅ ∈ x /\ ~(∅ = x).
 Proof.
-  exists (powerset empty_set).
+  exists (powerset ∅).
   split.
   - (* Proof that empty_set ∈ powerset empty_set *)
     apply powerset_spec.  (* To show empty_set ∈ powerset empty_set, we need to show empty_set ⊆ empty_set *)
@@ -27,7 +31,7 @@ Proof.
     apply Hx.             (* This is trivially true by hypothesis Hx *)
   - (* Proof that ~(empty_set = powerset empty_set) *)
     intro H_eq.           (* Assume for contradiction that empty_set = powerset empty_set *)
-    assert (empty_set ∈ empty_set). (* We know empty_set ∈ powerset empty_set from the first part of the proof.
+    assert (∅ ∈ ∅). (* We know empty_set ∈ powerset empty_set from the first part of the proof.
                                      If powerset empty_set = empty_set, then empty_set must be an element of itself. *)
     {
       rewrite H_eq.        (* Use the assumption H_eq to rewrite empty_set with powerset empty_set *)
@@ -40,4 +44,40 @@ Proof.
     apply empty_spec in H. (* Now we have empty_set ∈ empty_set (from H) and ~empty_set ∈ empty_set (from empty_spec).
                              This is a contradiction. *)
     apply H.               (* Apply the contradiction *)
+Qed.
+
+Axiom regularity : forall x, ((x <> ∅) -> exists y, (y ∈ x /\ y ∩ x = ∅)).
+Axiom union : forall f, exists a, forall y, forall x, ((x ∈ y /\ y ∈ f) -> x ∈ a).
+
+Theorem no_set_is_its_own_member : ~(exists x, x ∈ x).
+Proof.
+  intro.
+  destruct H.
+  specialize regularity with (x := x).
+  intros.
+  assert (x <> ∅).
+  {
+    intro.
+    specialize empty_spec with (x := x).
+    intros.
+    rewrite H1 in *.
+    apply H2.
+    apply H.
+  }
+  apply H0 in H1. clear H0.
+  destruct H1.
+  destruct H0.
+  pose proof pairing.
+  pose proof union.
+Admitted.
+
+Theorem no_set_of_all_sets : ~(exists x, forall y, y ∈ x).
+Proof.
+  intro.
+  destruct H.
+  specialize H with (y := x).
+  pose proof no_set_is_its_own_member.
+  apply H0. clear H0.
+  exists x.
+  apply H.
 Qed.
